2bc6ed8103102b0b0312231b90cbdd2f9abc9d3c
Ming Liu <liu.ming50@gmail.com>
2026-05-25 11:42:07 +0200

    Support ubuntu 24.04
    Signed-off-by: Ming Liu <liu.ming50@gmail.com>

--- start.sh
+++ start.sh
@@ -7,34 +7,34 @@ fi
 
 # create a group with a proper id, in case it doesn't exist
 if ! cat /etc/group | grep ":$2:" > /dev/null 2>&1 ; then
-    groupadd -g $2 builder 2>/dev/null
+    groupadd -g $2 ubuntu 2>/dev/null
 fi
 
 # Add uid, gid for builder, in case it doesn't exist
 if ! cat /etc/passwd | grep ":$1:" > /dev/null 2>&1 ; then
-    useradd -m -u $1 -g $2 -s /bin/bash builder 2>/dev/null
+    useradd -m -u $1 -g $2 -s /bin/bash ubuntu 2>/dev/null
 fi
 
 # Use bash as the default shell
 ln -sf /bin/bash /bin/sh
 
 # Zap the password for builder
-[ -e /etc/shadow ] && sed -i 's%^builder:.:%builder::%' /etc/shadow
-[ -e /etc/passwd ] && sed -i 's%^builder:x:%builder::%' /etc/passwd
+[ -e /etc/shadow ] && sed -i 's%^ubuntu:.:%ubuntu::%' /etc/shadow
+[ -e /etc/passwd ] && sed -i 's%^ubuntu:x:%ubuntu::%' /etc/passwd
 
-echo -e "\n# Builder privilege specification\nbuilder ALL=NOPASSWD: ALL" >> /etc/sudoers
+echo -e "\n# Builder privilege specification\nubuntu ALL=NOPASSWD: ALL" >> /etc/sudoers
 
 [ -c "$(tty)" ] && chmod a+rw $(tty)
 
 cp -rp /opt/yocto/docker/home /
 
 # Set correct variables according to the passed parameters
-sed -i "s#@@BUILD_DIR@@#$3#" /home/builder/.bashrc
+sed -i "s#@@BUILD_DIR@@#$3#" /home/ubuntu/.bashrc
 
 # Start builder
 if [ "$7" != "" ] ; then
-    su - builder -c "$7"
+    su - ubuntu -c "$7"
     exit $?
 fi
 
-su - builder
+su - ubuntu
